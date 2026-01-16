353039c5b8a589110ababefb686c9757d263bb33
rithaven <rita.git@gmail.com>
2026-01-16 16:26:10 +0200

    feat: Added Scenario Outline

--- src/test/java/awesomecucumber/MyStepdefs.java
+++ src/test/java/awesomecucumber/MyStepdefs.java
@@ -1,24 +1,51 @@
 package awesomecucumber;
 
+
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.Assert.assertEquals;
 
 public class MyStepdefs {
+    @Given("I'm an existing customer of xyz app")
+    public void iMAnExistingCustomerOfXyzApp() {
 
-    @Given("I'm dummy")
-    public void i_m_dummy() {
-        System.out.println("Given step executed");
     }
 
-    @When("I do dummy things")
-    public void i_do_dummy_things() {
-        System.out.println("When step executed");
+    @When("I provide my credentials")
+    public void iProvideMyCredentials() {
+
     }
 
-    @Then("dummy things happen")
-    public void dummy_things_happen() {
-        System.out.println("Then step executed");
+    @Then("I'm logged in")
+    public void iMLoggedIn() {
+
+    }
+
+    @Then("I'm logged in and greeted with a message {string}")
+    public void iMLoggedInAndGreetedWithAMessage(String arg0) {
+
+    }
+
+    @Given("My username is {string} and password is {string}")
+    public void myUsernameIsAndPasswordIs(String arg0, String arg1) {
+    }
+
+
+    private int balance; // must be class-level
+
+    @Given("my account balance is ${int}")
+    public void my_account_balance_is(int openingBalance) {
+        balance = openingBalance;
+    }
+
+    @When("I withdraw ${int}")
+    public void i_withdraw(int withdrawalAmount) {
+        balance -= withdrawalAmount;
+    }
+
+    @Then("the account balance should be ${int}")
+    public void the_account_balance_should_be(int expectedBalance) {
+        assertEquals(expectedBalance, balance);
     }
 }
-
--- src/test/java/awesomecucumber/dummy.feature
+++ src/test/java/awesomecucumber/dummy.feature
@@ -1,6 +1,36 @@
-Feature: Dummy feature
-  Scenario: Dummy scenario
-    Given I'm dummy
-    When I do dummy things
-    Then dummy things happen
+Feature: user login
 
+  #1 - Send credentials as a Data Table.
+  Scenario: login is successful
+    Given I'm an existing customer of xyz app
+    | username | password |
+    | john     | john123 |
+    When I provide my credentials
+    Then I'm logged in
+
+  #2 - Concrete outcome in the "Then" step in the form of a message "Hi joh"
+  Scenario: login is successful
+    Given I'm an existing customer of xyz app
+      | username | password |
+      | john     | john123 |
+    When I provide my credentials
+    Then I'm logged in and greeted with a message "Hi john"
+
+  #3 - Send credentials as parameters
+  Scenario Outline: login is successful
+    Given My username is "<username>" and password is "<password>"
+    When I provide my credentials
+    Then I'm logged in and greeted with a message "Hi John"
+    Examples:
+      | username | password |
+      | john     | john123 |
+  @QA
+  Scenario Outline: Dummy scenario
+    Given my account balance is $<opening_balance>
+    When I withdraw $<withdrawal_amount>
+    Then the account balance should be $<closing_balance>
+
+    Examples:
+      | opening_balance | withdrawal_amount | closing_balance |
+      | 100             | 50                | 50              |
+      | 50              | 50                | 0               |
